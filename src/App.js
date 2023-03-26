c7337053d565190d3d7858164ad996b45e872049
HaiNguyen <bongnghin5@gmail.com>
2023-03-26 08:20:09 +0700

    done the UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import Navbar from './components/Navbar'
-import InputPage from './pages/inputPage';
-import InputProcessingPage from './pages/inputProcessingPage';
-import OutputPage from './pages/outputPage';
-import './App.scss';
+import Navbar from "./components/Navbar";
+import InputPage from "./pages/inputPage";
+import InputProcessingPage from "./pages/inputProcessingPage";
+import OutputPage from "./pages/outputPage";
+import "./App.scss";
 
-
+//TODO: adding the excel button
+//TODO: adding the drag and drop
+//TODO: user hint for inputs
 function App() {
   return (
     <div className="App">
       <Navbar />
       <InputPage />
     </div>
-
   );
 }
 
--- src/pages/inputPage/index.jsx
+++ src/pages/inputPage/index.jsx
@@ -2,8 +2,46 @@ import React from 'react';
 import "./style.scss";
 import SpecialPlayerInput from "../../components/specialPlayerInput";
 import Input from "../../components/input";
+import ExcelImage from '../../images/excel.png'
+import { saveAs } from 'file-saver';
+import Excel from 'exceljs';
+
 
 export default function InputPage() {
+
+    const downloadExcel = () => {
+        const workbook = new Excel.Workbook();
+        const sheet = workbook.addWorksheet('Sheet 1');
+
+        // Add data to the worksheet here
+
+        workbook.xlsx.writeBuffer().then((buffer) => {
+            const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+            saveAs(blob, 'filename.xlsx');
+        });
+    }
+
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+
+        const file = event.dataTransfer.files[0];
+
+        console.log(file.name);
+    }
+
+    const handleOnDragEnter = (event) => {
+        event.preventDefault()
+
+        event.target.classList.add("dragging")
+    }
+
+    const handleOnDragLeave = (event) => {
+        event.preventDefault()
+
+        event.target.classList.remove("dragging")
+    }
+
     return (
         <div className="input-page">
             <p class='header-text'>Enter information about your problem</p>
@@ -28,6 +66,15 @@ export default function InputPage() {
                 <div className="row">
                     <Input message='Player payoff function'/>
                 </div>
+            </div>
+            <div className="excel-btn" onClick={downloadExcel}>
+                <p>Get Excel Template</p>
+                <img src={ExcelImage} alt="" />
+            </div>
+            <div className="drag-area" onDrop={handleDrop} onDragEnter={handleOnDragEnter} onDragLeave={handleOnDragLeave}>
+                <p class='drag-text'>Drag and drop a file here</p>
+                <label htmlFor="select-file" id='select-file-label'>Choose a file</label>
+                <input type="file" id="select-file"/>
             </div>
         </div>
     )
