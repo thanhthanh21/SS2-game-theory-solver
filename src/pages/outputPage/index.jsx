f8e91f942ccedc9b813d651569564c2e7fd6b9af
HaiNguyen <bongnghin5@gmail.com>
2023-03-31 21:15:58 +0700

    add data

--- src/pages/outputPage/index.jsx
+++ src/pages/outputPage/index.jsx
@@ -6,6 +6,70 @@ import ExcelImage from '../../images/excel.png'
 import GraphImage from '../../images/graph1.png'
 
 export default function OutputPage() {
+    const data = {
+        problem: {
+            name: "Oil Price Problem",
+            fitness: 15821810.1241,
+            players: {
+                problem: {
+                    name: "Oil Price Problem",
+                    fitness: 15821810.1241,
+                    players: [
+                        {
+                            name: "Player 1",
+                            choosenStrategy: "Strategy 3",
+                            payoffValue: 0.20028350308120002
+                        },
+                        {
+                            name: "Player 2",
+                            choosenStrategy: "Strategy 2",
+                            payoffValue: 0.44126170431689626
+                        },
+                        {
+                            name: "Player 3",
+                            choosenStrategy: "Strategy 5",
+                            payoffValue: 0.8663174343245001
+                        },
+                        {
+                            name: "Player 4",
+                            choosenStrategy: "Strategy 1",
+                            payoffValue: 0.9092766191717305
+                        },
+                        {
+                            name: "Player 5",
+                            choosenStrategy: "Strategy 1",
+                            payoffValue: 0.8671769111013774
+                        },
+                        {
+                            name: "Player 6",
+                            choosenStrategy: "Strategy 2",
+                            payoffValue: 0.0685257356799478
+                        },
+                        {
+                            name: "Player 7",
+                            choosenStrategy: "Strategy 2",
+                            payoffValue: 0.32624460862736093
+                        },
+                        {
+                            name: "Player 8",
+                            choosenStrategy: "Strategy 4",
+                            payoffValue: 0.07888482214902304
+                        },
+                        {
+                            name: "Player 9",
+                            choosenStrategy: "Strategy 2",
+                            payoffValue: 0.6083824549909775
+                        },
+                        {
+                            name: "Player 10",
+                            choosenStrategy: "Strategy 3",
+                            payoffValue: 0.006841291194771726
+                        }
+                    ]
+                }
+            }
+        }
+    }
     return (
         <div className='output-page'>
             <p class='header-text'>Oil Price Problem</p>
