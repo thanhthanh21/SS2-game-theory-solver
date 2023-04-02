9cdc33c687832b3b197085bca26811d7093a81ee
LamTrieu286 <115156880+LamTrieu286@users.noreply.github.com>
2023-04-02 19:35:22 +0700

    done

--- src/components/PlayerResult/index.jsx
+++ src/components/PlayerResult/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.scss";
-export default function PlayerResult() {
-    return(
-        <div className="grid-item-container">
-        <div className="column">#1</div>
-        <div className="column player-name">Player Name</div>
-        <div className="column">Choosen strategy name</div>
-        <div className="column">Payoff value</div>
+
+export default function PlayerResult(props) {
+  const { player } = props;
+
+  return (
+    <div className="grid-item-container">
+      <div className="column">{player.idex}</div>
+      <div className="column player-name">{player.name}</div>
+      <div className="column">{player.choosenStrategy}</div>
+      <div className="column">{player.payoffValue}</div>
     </div>
-    )
+  );
 }
